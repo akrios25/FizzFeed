b260455362ec8ea15ebc8ef2c648447bafe93641
Kevin Toro <kevintoro@ac.c4q.nyc>
2017-11-17 23:33:27 -0500

    Initial  Quiz app commit

---
+++ Desktop/AccessCodeprojects/FizzFeed/app/src/main/java/nyc/c4q/fizzfeed/Quiz01.java
@@ -0,0 +1,16 @@
+package nyc.c4q.fizzfeed;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by c4q on 11/16/17.
+ */
+
+public class Quiz01 extends AppCompatActivity {
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(R.layout.quiz_01_view);
+    }
+}
---
+++ Desktop/AccessCodeprojects/FizzFeed/app/src/main/java/nyc/c4q/fizzfeed/Quiz02.java
@@ -0,0 +1,9 @@
+package nyc.c4q.fizzfeed;
+
+/**
+ * Created by c4q on 11/16/17.
+ */
+
+public class Quiz02 {
+
+}
